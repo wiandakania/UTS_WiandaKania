0102f07e718257794dc58d244f6ae4d28e2de9e3
wiandakania <wiandakaniaa@gmail.com>
2026-05-08 14:06:45 +0800

    Create pelanggan.java

---
+++ pelanggan.java
@@ -0,0 +1,14 @@
+public class pelanggan extends user{
+    private String tanggal;
+
+public pelanggan(String idUser, String nama, String tanggal){
+    super(idUser, nama);
+    this.tanggal = tanggal;
+}
+public void pilihMenu(){
+    System.out.println("Tampilkan menu yang bisa dipesan");
+}
+public void pesanMenu(){
+    System.out.println("Menu yang dipesan");
+}
+}
