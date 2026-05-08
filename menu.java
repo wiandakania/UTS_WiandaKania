7566a18750a19721dd93a8233706956783fb3d4c
wiandakania <wiandakaniaa@gmail.com>
2026-05-08 14:08:04 +0800

    Create menu.java

---
+++ menu.java
@@ -0,0 +1,15 @@
+public class menu {
+    private String namaMakanan;
+    private double hargaMakanan;
+
+public menu(String namaMakanan, double hargaMakanan){
+    this.namaMakanan = namaMakanan;
+    this.hargaMakanan = hargaMakanan;
+}
+
+public void tampilkanMenu(){
+    System.out.println("Tampilkan semua menu");
+}
+
+public void 
+}
