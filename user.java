88a7e8420f38be3fb28ea05e32aa43b60e5583e7
wiandakania <wiandakaniaa@gmail.com>
2026-05-08 14:05:47 +0800

    Create user.java

---
+++ user.java
@@ -0,0 +1,16 @@
+ublic class user {
+    private String idUser;
+    private String nama;
+
+public user(String idUser, String nama){
+    this.idUser = idUser;
+    this.nama = nama;
+}
+public void login(){
+    System.out.println("Berhasil Login");
+}
+
+public void logout(){
+    System.out.println("Berhasil Keluar");
+}
+}
