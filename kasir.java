c706dab5da6d9a6bbeb91435ff92cbb97ce54de7
wiandakania <wiandakaniaa@gmail.com>
2026-05-08 14:07:37 +0800

    Create kasir.java

---
+++ kasir.java
@@ -0,0 +1,16 @@
+public class kasir extends user {
+    private String shift;
+
+    public kasir(String idUser, String nama, String shift) {
+        super(idUser, nama);
+        this.shift = shift;
+    }
+
+    public void cekStok() {
+        System.out.println("Tampilkan menu yang tersedia");
+    }
+
+    public void cekPembayaran() {
+        System.out.println("Tampilkan Pembayaran");
+    }
+}
